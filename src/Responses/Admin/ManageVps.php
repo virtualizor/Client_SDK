acd4b4701971d072d00ee0c21f0a7b519afe5bf9
غلامرضا بهشتیان <amintado@gmail.com>
2021-12-18 11:33:43 +0330

    classify managevps method

--- src/Responses/Admin/ManageVps.php
+++ src/Responses/Admin/ManageVps.php
@@ -6,10 +6,11 @@
 namespace YiiMan\VirtualizorSdk\Responses\Admin;
 
 /**
+ *
+ *
  * @property VS $vs_info
  */
 class ManageVps extends DoneResponse
 {
     public $vs_info='class:YiiMan\VirtualizorSdk\Responses\Admin\VS';
-
 }
